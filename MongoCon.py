d47fd09d3dc4590f70cf2e1cd68406d3770643c6
Wolfgang <wvitzthum@gmail.com>
2019-02-01 18:12:38 +0000

    moved API file to own repository

--- MongoCon.py
+++ MongoCon.py
@@ -3,7 +3,7 @@ from bson.json_util import dumps
 
 class MongoCon:
     def __init__(self):
-        self.client = MongoClient("192.168.8.31", 27017)  # type: MongoClient
+        self.client = MongoClient("0.0.0.0", 27017)  # type: MongoClient
         self.db = self.client.plasmid
         self.colServ = self.db.servers
 
@@ -12,7 +12,7 @@ class MongoCon:
         print(self.client.list_database_names())
 
     def getServer(self, name):
-        client = MongoClient('192.168.31.103', 27017)
+        client = MongoClient('0.0.0.0', 27017)
         db = client.plasmid
         collection = db.servers
         result = collection.find_one({'host': name})
